5f9a6ff3beef3e491109dea49748f0f0b70403d3
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-08-18 01:43:50 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9999" // Default port if not set
+	}
+
+	addr := "0.0.0.0:" + port
+	server, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer server.Close()
+	log.Println("Server is running on:", addr)
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Println("Failed to accept conn.", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("New connection from: %s\n", conn.RemoteAddr())
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		content := scanner.Text()
+		fmt.Printf("[%s]: %s\n", conn.RemoteAddr(), content)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading from connection %s: %v\n", conn.RemoteAddr(), err)
+	}
+}
